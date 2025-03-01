7d07e55f58a19aaf1fd00488d845b01b097ae70b
Italo Canturil <italo.cantur@gmail.com>
2025-03-01 17:16:25 -0300

    refactor(blog): add min height

--- apps/portfolio/pages/blog/[...slug].vue
+++ apps/portfolio/pages/blog/[...slug].vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="flex flex-col justify-center h-screen prose">
+  <main class="flex flex-col justify-center min-h-screen prose">
     <ContentDoc v-slot="{ doc }">
       <ContentRenderer :value="doc"/>
     </ContentDoc>
