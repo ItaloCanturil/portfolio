8b67025ae5cafe5c447bcd5f96517d95b3971c63
Italo Canturil <italo.cantur@gmail.com>
2025-03-18 17:31:33 -0300

    refactor: adjust query to `BlogList` component

--- apps/portfolio/components/BlogList.vue
+++ apps/portfolio/components/BlogList.vue
@@ -1,22 +1,23 @@
 <script setup lang="ts">
-import type { QueryBuilderParams } from '@nuxt/content'
+  import type { QueryBuilderParams } from '@nuxt/content'
 
-defineProps<{
-  query?: QueryBuilderParams
-}>()
+  defineProps<{
+    query?: QueryBuilderParams
+  }>()
 
 </script>
 
 <template>
-  <ContentList :quer="query" v-slot="{ list }">
-    <NuxtLink :href="article._path" v-for="article in list" :key="article._path" class="card flex flex-col rounded-lg p-4 border my-2 h-36">
-        <div class="flex items-center justify-between">
-          <span class="dark:text-isabelline-300 text-eerie_black-700">{{ formatDate(article.publishedAt) }}</span>
+  <ContentList :query="query" v-slot="{ list }">
+    <NuxtLink :href="article._path" v-for="article in list" :key="article._path"
+      class="card flex flex-col rounded-lg p-4 border my-2 h-36">
+      <div class="flex items-center justify-between">
+        <span class="dark:text-isabelline-300 text-eerie_black-700">{{ formatDate(article.publishedAt) }}</span>
 
-          <IconArrowRight :size="14" class="-rotate-45 icon"/>
-        </div>
-        <h2>{{ article.title }}</h2>
-        <p>{{ article.description }}</p>
+        <IconArrowRight :size="14" class="-rotate-45 icon" />
+      </div>
+      <h2>{{ article.title }}</h2>
+      <p>{{ article.description }}</p>
     </NuxtLink>
   </ContentList>
 </template>
@@ -25,5 +26,4 @@ defineProps<{
 .card:hover .icon {
   @apply rotate-0 duration-150 ease-in
 }
-
 </style>
--- apps/portfolio/pages/index.vue
+++ apps/portfolio/pages/index.vue
@@ -1,42 +1,42 @@
 <script setup lang="ts">
-import type { QueryBuilderParams } from '@nuxt/content';
+  import type { QueryBuilderParams } from '@nuxt/content';
 
-type ProjectType = {
-  hasLink: boolean,
-  link: string,
-  title: string,
-  badge: string | string[],
-  description: string,
-  across: string[],
-};
+  type ProjectType = {
+    hasLink: boolean,
+    link: string,
+    title: string,
+    badge: string | string[],
+    description: string,
+    across: string[],
+  };
 
-const query: QueryBuilderParams = { path: '/blog', limit: 2, sort: [{ date: -1}] };
+  const query: QueryBuilderParams = { path: '/blog', limit: 3, sort: [{ publishedAt: -1 }] };
 
-const projects: ProjectType[] = [
-  {
-    hasLink: true,
-    link: "beauty-reservation.vercel.app",
-    title: "Beauty Calendar",
-    description: "A Calendar for Beauty Professionals admin and client with Google Calendar integrated",
-    badge: "Indie project",
-    across: ['Typescript', 'Supabase (Postgre SQL)', 'PrimeVue/UI', 'Nuxt']
-  },
-  {
-    hasLink: true,
-    link: "feelingdiary.com.br",
-    title: "Emotion Management",
-    description: "A tracker of emotions and thoughs",
-    badge: "Indie project",
-    across: ['PWA', 'Quasar', 'Typescript']
-  },
-]
+  const projects: ProjectType[] = [
+    {
+      hasLink: true,
+      link: "beauty-reservation.vercel.app",
+      title: "Beauty Calendar",
+      description: "A Calendar for Beauty Professionals admin and client with Google Calendar integrated",
+      badge: "Indie project",
+      across: ['Typescript', 'Supabase (Postgre SQL)', 'PrimeVue/UI', 'Nuxt']
+    },
+    {
+      hasLink: true,
+      link: "feelingdiary.com.br",
+      title: "Emotion Management",
+      description: "A tracker of emotions and thoughs",
+      badge: "Indie project",
+      across: ['PWA', 'Quasar', 'Typescript']
+    },
+  ]
 </script>
 
 <template>
   <div class="flex flex-col h-full justify-center">
     <div class="min-h-20 mt-4 flex flex-col gap-2">
       <div>
-        <h1 class="text-xl">{{ $t('hello')}}</h1>
+        <h1 class="text-xl">{{ $t('hello') }}</h1>
         <p>{{ $t('onboarding') }}</p>
       </div>
       <UITechStack />
@@ -46,21 +46,22 @@ const projects: ProjectType[] = [
     </div>
 
     <div class="divider"></div>
-    
+
     <section>
       <p class="text-xl">{{ $t('recentSection') }}</p>
 
       <div class="flex items-center gap-2">
-        <BlogList :query="query"/>
+        <BlogList :query="query" />
       </div>
     </section>
-    
+
     <div class="divider"></div>
-    
+
     <section>
       <p class="text-xl">{{ $t('projectSection') }}</p>
       <div class="flex flex-col gap-4">
-        <div class="card flex flex-col rounded-lg p-4 border my-2 gap-4" v-for="project in projects" :key="project.title">
+        <div class="card flex flex-col rounded-lg p-4 border my-2 gap-4" v-for="project in projects"
+          :key="project.title">
           <div class="flex items-center justify-between">
             <div class="font-bold flex flex-col">
               {{ project.title }}
@@ -68,7 +69,7 @@ const projects: ProjectType[] = [
             </div>
 
             <NuxtLink v-if="project.hasLink" :href="project.link" external class="flex items-center gap-1">
-              <IconExternalLink :size="16"/>
+              <IconExternalLink :size="16" />
               External link
             </NuxtLink>
           </div>
